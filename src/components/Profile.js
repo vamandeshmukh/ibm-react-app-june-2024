2a1387b3cb18ff6a448f2e83e1eba9f0d8b4ee9e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-06-11 17:34:46 +0530

    reactjs-intro

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,38 @@
-const Profile = () => {
+import { useEffect, useState } from "react";
+import { getProfile } from "../services/UserService";
 
-    
+const Profile = () => {
+    console.log('Profile');
+
     const [userProfile, setUserProfile] = useState('');
 
+    useEffect(() => {
+        getProfile(2)
+            .then((response) => {
+                console.log(response.data);
+                setUserProfile(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    console.log('Done!');
 
     return (
         <>
             <h1>User Profile Component</h1>
             <>
-
+                {userProfile &&
+                    <>
+                        <h3>{userProfile.name}</h3>
+                        <p>{userProfile.name}</p>
+                        <p>{userProfile.email}</p>
+                        <p>{userProfile.address.city}</p>
+                        <p>{userProfile.phone}</p>
+                        <p>{userProfile.company.name}</p>
+                    </>
+                }
             </>
         </>
     );
