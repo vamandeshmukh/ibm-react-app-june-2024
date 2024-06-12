5bb85ee11c361ff7882efff1f76656b3bc21bb0e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-06-12 15:06:54 +0530

    WriteBlog component

---
+++ src/components/WriteBlog.js
@@ -0,0 +1,14 @@
+
+
+const WriteBlog = () => {
+
+    return (
+        <>
+            <h1>Write a Blog</h1>
+            <p>Express yourself</p>
+        </>
+    );
+
+};
+
+export default WriteBlog;
