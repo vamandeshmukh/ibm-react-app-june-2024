0237e1a1abef5d3f133fe439efd1d3aec6e3c616
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-06-12 15:07:25 +0530

    AppRoutes component

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+// AppRoutes.js
+
+import { BrowserRouter, Route, Routes, Link, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import Profile from "./Profile";
@@ -7,6 +9,7 @@ import Page404 from "./Page404";
 import ToolBar from "./ToolBar";
 import BlogDetails from "./BlogDetails";
 import BlogList from "./BlogList";
+import WriteBlog from "./WriteBlog";
 
 const AppRoutes = () => {
 
@@ -17,12 +20,13 @@ const AppRoutes = () => {
                     <ToolBar />
                 </>
                 <Routes>
+                <Route path="write-blog" element={<WriteBlog />} />
                     <Route path="blog-list" element={<BlogList />} />
                     <Route path="blog-details/:id" element={<BlogDetails />} />
                     <Route path="login" element={<Login />} />
                     <Route path="register" element={<Register />} />
                     <Route path="profile" element={<Profile />} />
-                    <Route path="home" element={<Home />} />
+                    <Route path="home" element={<Navigate to="/" replace />} />
                     <Route exact path="/" element={<Home />} />
                     <Route path="*" element={<Page404 />} />
 
