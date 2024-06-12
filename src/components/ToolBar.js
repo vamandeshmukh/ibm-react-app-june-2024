a7628d3edf296f0299f5fedeb58c78d6d4b600da
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-06-12 12:24:25 +0530

    react-js

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -4,6 +4,7 @@ const ToolBar = () => {
 
     return (
         <>
+            <Link to="blog-list">Blog List</Link>
             <Link to="login">Login</Link>
             <Link to="home">Home</Link>
             <Link to="register">Register</Link>
--- src/services/BlogService.js
+++ src/services/BlogService.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const blogUrl = '';
+const blogUrl = 'https://jsonplaceholder.typicode.com/posts/';
+
 const getAllBlogs = () => {
     console.log('getAllBlogs');
     return axios.get(blogUrl);
 };
 
-const getBlogById = (id) => { };
+const getBlogById = (id) => {
+    console.log(id);
+    return axios.get(`${blogUrl}/${id}`);
+};
 
 export { getAllBlogs, getBlogById };
