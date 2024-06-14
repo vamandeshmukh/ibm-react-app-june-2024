28cfc3c79603ea3354705603196877d43081a3e5
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-06-14 11:35:46 +0530

    react-js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,14 +1,14 @@
+// steps in using redux store -
+
+// install libraries
+// create store - Store.js
+// provide store to the app - index.js 
+// create slices - COmponentNameSlice.js 
+// send data to store using dispatch and reducer methods - in components  
+// receive data from store using selector - in components 
+
 import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from './UserSlice';
-
-// steps to use redux in react
-
-// 1. create store 
-// provide store to index.js 
-// create slices for each components 
-// call slice methods in components to send data to store 
-// call dispatchers in components 
-// selectors to get data from store 
 
 const store = configureStore({
     reducer: {
@@ -20,6 +20,33 @@ const store = configureStore({
 });
 
 export default store;
+
+
+
+
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import UserReducer from './UserSlice';
+
+// // steps to use redux in react
+
+// // 1. create store 
+// // provide store to index.js 
+// // create slices for each components 
+// // call slice methods in components to send data to store 
+// // call dispatchers in components 
+// // selectors to get data from store 
+
+// const store = configureStore({
+//     reducer: {
+//         user: UserReducer,
+//         // blog: BlogReducer,
+//         // other: OtherReducer 
+//     }
+
+// });
+
+// export default store;
 
 
 
